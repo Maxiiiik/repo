9df8109028031bab82957e5c9e509107d024d7bf
Maxi <shuprudkomax00@gmail.com>
2021-02-26 13:32:09 +0200

    added some really great code

---
+++ numwrds.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+int main()
+{
+    string str;
+    int numwrds = 0;
+    cout << "Enter the sentence : ";
+    getline(cin, str);
+    for (size_t i = 0; i <= str[i]; i++)
+    {
+        if (str[i] == ' ')
+            numwrds++;
+    }
+    cout << "Total words = " << numwrds + 1;
+
+
+
+
+
+    cout << endl;
+    system("pause");
+    return 0;
+}
