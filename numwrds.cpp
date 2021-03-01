0cd1aaefb18bc3bfde49845ed8263a55ab69270f
Maxi <shuprudkomax00@gmail.com>
2021-03-01 14:30:47 +0200

    [2.3] Edit numwrds

--- numwrds.cpp
+++ numwrds.cpp
@@ -6,22 +6,29 @@
 //one more nice code block
 using namespace std;
 
-int main(int argc , char * argv[])
+int main(int argc, char* argv[])
 {
-	for(size_t i = 0; i < argc ; i++)
-	{
-		if(strcmp(argv[i],"-h"== 0)
-		{
-			cout << "This program WORK"
-			return 0;
-		}
-		
-	}
-	
-	
+    char arr[100];
+
+    for (size_t i = 0; i < argc; i++)
+    {
+        if (strcmp(argv[i], "-h") == 0)
+        {
+            cout << "This program WORK";
+                return 0;
+        }
+        if (strcmp(argv[i], "-c") == 0)
+        {
+            cout << "str : " << arr << endl;
+            cout << "Number of words : " << argc - 2 << endl;
+
+        }
+    }
+
+
     string str;
-    int numwrds = 0;
-    cout << "Enter the sentence : ";
+        int numwrds = 0;
+        cout << "Enter the sentence : ";
     getline(cin, str);
     for (size_t i = 0; i <= str[i]; i++)
     {
